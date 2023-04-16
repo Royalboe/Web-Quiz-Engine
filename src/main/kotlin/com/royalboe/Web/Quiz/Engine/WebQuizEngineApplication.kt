b279e4096c885c9ec54108cdf15a7b61396fd52e
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-04-16 18:51:26 +0100

    First task worked

--- src/main/kotlin/com/royalboe/Web/Quiz/Engine/WebQuizEngineApplication.kt
+++ src/main/kotlin/com/royalboe/Web/Quiz/Engine/WebQuizEngineApplication.kt
@@ -2,9 +2,42 @@ package com.royalboe.Web.Quiz.Engine
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import java.util.concurrent.ConcurrentHashMap
+
+
+class Quiz(val title: String, val text: String, val options: List<String>) {
+}
+
 
 @SpringBootApplication
-class WebQuizEngineApplication
+@RestController
+class WebQuizEngineApplication {
+	
+	val title = "The Java Logo"
+	val text = "What is depicted on the Java logo?"
+	val options: List<String> = listOf("Robot", "Tea leaf", "Cup of coffee", "Bug")
+
+	val quizzes = listOf(
+		Quiz(title, text, options)
+	)
+
+	@GetMapping("/api/quiz")
+	fun getQuiz(): ResponseEntity<Quiz> {
+		return ResponseEntity.ok(quizzes[0])
+	}
+
+	@PostMapping("/api/quiz")
+	fun postQuiz(@RequestParam answer: Int): String {
+		return if (answer == 2) {
+			"{\"success\":true,\"feedback\":\"Congratulations, you're right!\"}"
+		} else {
+			"{\"success\":false,\"feedback\":\"Wrong answer! Please, try again.\"}"
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<WebQuizEngineApplication>(*args)
